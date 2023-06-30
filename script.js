9c257a9df7878ff3ee68d1a9d0306ad8ae27f612
Oscar Hernández <ohdz202@gmail.com>
2023-06-30 15:35:28 -0600

    Clase 03 completada!

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ console.log(btn)
 
 const crearTarea = (evento) => {
     evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
-}
+    const input = document.querySelector("[data-form-input]"); //seleccionando lo que el usuario escribio en el input
+    const value = input.value; // Capturando el valor del input
+    const list = document.querySelector("[data-list]");
+    const nuevaTarea = document.createElement('li');
+    nuevaTarea.classList.add("card");
+    input.value = "";
+    const contenido = 
+    `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>` // Colocar el texto en el div
+    nuevaTarea.innerHTML = contenido; 
 
+    list.appendChild(nuevaTarea);
+    console.log(contenido);
+};
 btn.addEventListener("click", crearTarea);
